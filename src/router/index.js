8c053ec3eeb8a87131f79122831993cbdbcef295
xiaochen101 <2358057786@qq.com>
2026-04-19 10:09:19 +0800

    修改路由模式为历史模式

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,8 @@ const UserInfo = () => import('@/views/my/index.vue')
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode: 'history',
+  base: './',
   routes: [
     {
       path: '/login',
