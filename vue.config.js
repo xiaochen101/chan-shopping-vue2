5dbed4af08cf0764ca72121c3e16ee688d44d455
xiaochen101 <2358057786@qq.com>
2026-04-19 18:06:28 +0800

    恢复配置文件为默认

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './',
-  transpileDependencies: true,
-  devServer: {
-    historyApiFallback: true
-  }
+  transpileDependencies: true
 })
